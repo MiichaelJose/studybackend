90371cfa286a07f9f9f82bd7fbd11b9b522a4906
MiichaelJose <michaeljosecraft1@gmail.com>
2023-09-12 23:28:14 -0300

    junit-1

---
+++ testes/junit-1/src/main/java/Controllers/Usuario.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+public class Usuario {
+	private String nome;
+
+	private String senha;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+}
---
+++ testes/junit-1/src/test/java/ParaTestesDois.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ParaTestesDois {
+
+	@Test
+	public void testSomar() {
+		int resultado = 6;
+		assertEquals(6, resultado);
+	}
+}
